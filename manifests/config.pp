84788a67c441d563cfb7ef2e9906b8aaafc4b6f3
Paul Gomersbach <p.gomersbach@rely.nl>
2016-07-28 13:34:32 +0000

    prevent direct use of subclass

--- manifests/config.pp
+++ manifests/config.pp
@@ -3,6 +3,10 @@
 # This class is called from profile_mysql for service config.
 #
 class profile_mysql::config {
+  # prevent direct use of subclass
+  if $caller_module_name != $module_name {
+    fail("Use of private class ${name} by ${caller_module_name}")
+  }
 
   mysql::db { $::profile_mysql::dbname:
     user     => $::profile_mysql::dbuser,
--- manifests/install.pp
+++ manifests/install.pp
@@ -3,7 +3,10 @@
 # This class is called from profile_mysql for install.
 #
 class profile_mysql::install {
-
+  # prevent direct use of subclass
+  if $caller_module_name != $module_name {
+    fail("Use of private class ${name} by ${caller_module_name}")
+  }
 
   $override_options = {
   'mysqld' => {
