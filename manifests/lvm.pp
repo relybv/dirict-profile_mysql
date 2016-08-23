cc70b9b9b98ff90d0e49d4ef8e9844cfe2cc9589
P. Gomersbach <p.gomersbach@rely.nl>
2016-08-23 10:30:36 +0000

    force order in lvm

--- manifests/lvm.pp
+++ manifests/lvm.pp
@@ -9,43 +9,51 @@ class profile_mysql::lvm {
   }
 
   physical_volume { $::profile_mysql::dbvol:
-    ensure => present,
+    ensure  => present,
+    require => Class['::lvm'],
   }
 
   physical_volume { $::profile_mysql::nfsvol:
-    ensure => present,
+    ensure  => present,
+    require => Class['::lvm'],
   }
 
   volume_group { 'dbvg':
     ensure           => present,
     physical_volumes => $::profile_mysql::dbvol,
+    require          => Physical_volume[$::profile_mysql::dbvol],
   }
 
   volume_group { 'nfsvg':
     ensure           => present,
     physical_volumes => $::profile_mysql::nfsvol,
+    require          => Physical_volume[$::profile_mysql::nfsvol],
   }
 
   logical_volume { 'dblv':
     ensure       => present,
     volume_group => 'dbvg',
+    require      => Volume_group['dbvg'],
   }
 
   logical_volume { 'nfslv':
     ensure       => present,
     volume_group => 'nfsvg',
+    require      => Volume_group['nfsvg'],
   }
 
   filesystem { '/dev/dbvg/dblv':
     ensure  => present,
     fs_type => 'ext4',
     atboot  => true,
+    require => Logical_volume['dblv'],
   }
 
   filesystem { '/dev/nfsvg/nfslv':
     ensure  => present,
     fs_type => 'ext4',
     atboot  => true,
+    require => Logical_volume['nfslv'],
   }
 
   file { '/var/lib/mysql':
@@ -57,15 +65,17 @@ class profile_mysql::lvm {
 #  }
 
   mount { '/var/lib/mysql':
-    ensure => mounted,
-    device => '/dev/dbvg/dblv',
-    fstype => 'ext4',
+    ensure  => mounted,
+    device  => '/dev/dbvg/dblv',
+    fstype  => 'ext4',
+    require => [ File[ '/var/lib/mysql'], Filesystem['/dev/dbvg/dblv'] ],
   }
 
   mount { '/mnt/nfs':
-    ensure => mounted,
-    device => '/dev/nfsvg/nfslv',
-    fstype => 'ext4',
+    ensure  => mounted,
+    device  => '/dev/nfsvg/nfslv',
+    fstype  => 'ext4',
+    require => Filesystem['/dev/dbvg/nfslv'],
   }
 
 }
