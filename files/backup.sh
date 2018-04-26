9b3a80da9b0f8c2ad85f04c487afa42b6ce392ec
P. Gomersbach <p.gomersbach@rely.nl>
2018-04-26 05:49:36 +0000

    mysql backup lokatie gewijzigd naar /backup

--- files/backup.sh
+++ files/backup.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 echo "Starting backup job" > /root/bku.log
-bkupath="/var/lib/mysql-local/"
+#bkupath="/var/lib/mysql-local/"
+bkupath="/backup"
 now=$(date "+%d-%m-%Y")
 bkufile="${bkupath}all_databases-${now}-.sql"
 /usr/bin/mysqldump --all-databases > $bkufile
